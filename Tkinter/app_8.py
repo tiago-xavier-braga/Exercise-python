89c7956103259aae66621cda8cf04cdbd127a2a3
Tiago Xavier Braga <braga.tiagoxavier@gmail.com>
2020-07-13 10:01:23 -0300

    Delete app_8.py

--- Tkinter/app_8.py
+++
@@ -1,7 +0,0 @@
-from tkinter import *
-home_menu = Tk()
-home_menu['bg']= '#2D2F31'
-label_1 = Label(home_menu, text="I'm label !", font="Arial 40",width = 20)
-label_1.pack()
-home_menu.iconbitmap = "icone_3.ico"
-home_menu.mainloop()
